cef08c8f8c4059c373da0d73c27237eb490a0147
kittaro <83721094+kittaro@users.noreply.github.com>
2023-10-26 12:50:21 +0300

    Add files via upload
    lalala ZZZZ

---
+++ L7.pas
@@ -0,0 +1,17 @@
+﻿const m = 10;
+var a:array[1..m] of integer;
+i, long: integer;
+begin
+  for i:=1 to m do begin
+    a[i]:= random(-50, 50);
+    write(a[i],' ');
+  end;
+  writeln;
+  for i:=1 to m do begin
+    if i=10 then
+      break;
+    if a[i]<a[i+1] then
+      long:=long+1;
+    end;
+  write('Длина неубывающего участва = ', long)
+end.
---
+++ N12.pas
@@ -0,0 +1,14 @@
+﻿const m =20;
+var a:array[1..m] of integer;
+i, min,j, n: integer;
+begin
+  n:=m;
+  writeln ('Введите 20 элементов массива:  ');
+  for i:=1 to m do
+    read(a[i]);
+  for i:=1 to n do
+    if a[i]<=0 then
+      write(a[i], ' ' )
+    else
+      write('0 ',a[i],' ')
+end.
